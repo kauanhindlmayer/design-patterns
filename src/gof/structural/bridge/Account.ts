e23072e9aa461b36517b650705f02793b533996c
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2025-06-21 22:19:23 -0300

    bridge

---
+++ src/gof/structural/bridge/Account.ts
@@ -0,0 +1,22 @@
+import Password, { PasswordFactory } from "./Password";
+
+export default abstract class Account {
+  password: Password;
+
+  constructor(
+    readonly name: string,
+    readonly email: string,
+    readonly document: string,
+    password: string,
+    readonly passwordType: string
+  ) {
+    if (!name.match(/.+ .+/g)) throw new Error("Invalid name");
+    if (!email.match(/.+\@.+\..+/g)) throw new Error("Invalid email");
+    if (document.length !== 11) throw new Error("Invalid document");
+    this.password = PasswordFactory.create(passwordType, password);
+  }
+
+  passwordMatches(password: string): boolean {
+    return this.password.passwordMatches(password);
+  }
+}
