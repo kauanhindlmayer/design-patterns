a91c639c3c1b4f0124d7ec1115fd4fe4f9a3c87f
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2025-07-12 18:45:29 -0300

    command

---
+++ test/gof/behavioral/command/MakeTransfer.test.ts
@@ -0,0 +1,22 @@
+import BankAccount from "~/gof/behavioral/command/BankAccount";
+import { BankAccountRepositoryMemory } from "~/gof/behavioral/command/BankAccountRepository";
+import GetBalance from "~/gof/behavioral/command/GetBalance";
+import MakeTransfer from "~/gof/behavioral/command/MakeTransfer";
+
+test("Deve fazer uma transferência bancária", async function () {
+  const bankAccountRepository = new BankAccountRepositoryMemory();
+  bankAccountRepository.save(new BankAccount(1));
+  bankAccountRepository.save(new BankAccount(2));
+  const makeTransfer = new MakeTransfer(bankAccountRepository);
+  const input = {
+    fromBankAccountId: 1,
+    toBankAccountId: 2,
+    amount: 100,
+  };
+  await makeTransfer.execute(input);
+  const getBalance = new GetBalance(bankAccountRepository);
+  const outputA = await getBalance.execute(1);
+  expect(outputA.balance).toBe(-100);
+  const outputB = await getBalance.execute(2);
+  expect(outputB.balance).toBe(100);
+});
